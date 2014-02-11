30a1636bb140cecd178c339c9b83b95bc43907ab
fakeyou <ag.nanninga@gmail.com>
2014-02-11 16:29:16 +0100

    Leertaak 1 - opdracht 8
    "Maak een derde view aan die statistieken van de worpen weergeeft.
    Gebruik voor deze view een GridLayout."

--- leertaak1MvcDemo/src/DobbelsteenModel.java
+++ leertaak1MvcDemo/src/DobbelsteenModel.java
@@ -6,54 +6,69 @@ import java.util.*;
 // Zie paragraaf 30.2 voor de thread-safe implementatie.
 public class DobbelsteenModel
 {
-	private int waarde;
-	private ArrayList<ActionListener> actionListenerList = new ArrayList<ActionListener>();
+    private int waarde;
+    private int[] gegooideWaarden;
+    private ArrayList<ActionListener> actionListenerList = new ArrayList<ActionListener>();
 
-	public DobbelsteenModel()
-	{
-		waarde= (int)(Math.random()*6+1);
-	}
-	
+    public DobbelsteenModel()
+    {
+        waarde = (int)(Math.random()*6+1);
+        gegooideWaarden = new int[] { 0, 0, 0, 0, 0, 0};
+    }
+
     public int getWaarde()
     {
         return waarde;
-    }    
-	public void verhoog()
-	{
-		waarde++;
-	    if (waarde>6) waarde=1;
-	    
-	    // Merk op dat we de 3e String-parameter van de constructor van de ActionEvent niet invullen.
-	    // In dit geval zou je die kunnen gebruiken om de nieuwe dobbelsteenwaarde mee te geven
-	    // aan de ActionListener. Dan hoeft de ActionListener niet met e.getSource() weer naar
-	    // het model toe te gaan.
-	    processEvent( new ActionEvent( this, ActionEvent.ACTION_PERFORMED, null));
-	} 
-	public void verlaag()
-	{
-	    waarde--;
-	    if (waarde<1) waarde=6;
-	    processEvent( new ActionEvent( this, ActionEvent.ACTION_PERFORMED, null));
-	} 
-	public void gooi(){
-	    waarde= (int)(Math.random()*6+1);
-	    processEvent( new ActionEvent( this, ActionEvent.ACTION_PERFORMED, null));
-	}
-	
-	public void addActionListener( ActionListener l){
-		actionListenerList.add( l );
-	}
+    }
+    public int[] getGegooideWaarden() { return gegooideWaarden; }
+    public void verhoog()
+    {
+        waarde++;
+        if (waarde>6) waarde=1;
 
-	public void removeActionListener( ActionListener l){
-		if ( actionListenerList.contains( l ) )
-			actionListenerList.remove( l );
-	}
-	
-	private void processEvent(ActionEvent e){
-		// Hieronder gebruiken we het nieuwe Java "foreach" statement. 
-		// Lees het als: "for each ActionListener in actionListenerList do ..."
-		// Je kunt ook een for-lus of een iterator gebruiken, maar foreach is het elegantste.
-		for( ActionListener l : actionListenerList)
-			l.actionPerformed( e );
-	}
+        nieuweWaarde(waarde);
+
+        // Merk op dat we de 3e String-parameter van de constructor van de ActionEvent niet invullen.
+        // In dit geval zou je die kunnen gebruiken om de nieuwe dobbelsteenwaarde mee te geven
+        // aan de ActionListener. Dan hoeft de ActionListener niet met e.getSource() weer naar
+        // het model toe te gaan.
+        processEvent( new ActionEvent( this, ActionEvent.ACTION_PERFORMED, null));
+    }
+    public void verlaag()
+    {
+        waarde--;
+        if (waarde<1) waarde=6;
+
+        nieuweWaarde(waarde);
+
+        processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, null));
+    }
+    public void gooi(){
+        waarde = (int)(Math.random()*6+1);
+
+        nieuweWaarde(waarde);
+
+        processEvent( new ActionEvent( this, ActionEvent.ACTION_PERFORMED, null));
+    }
+
+    private void nieuweWaarde(int waarde) {
+        gegooideWaarden[waarde - 1]++;
+    }
+
+    public void addActionListener( ActionListener l){
+        actionListenerList.add( l );
+    }
+
+    public void removeActionListener( ActionListener l){
+        if ( actionListenerList.contains( l ) )
+            actionListenerList.remove( l );
+    }
+
+    private void processEvent(ActionEvent e){
+        // Hieronder gebruiken we het nieuwe Java "foreach" statement. 
+       // Lees het als: "for each ActionListener in actionListenerList do ..."
+        // Je kunt ook een for-lus of een iterator gebruiken, maar foreach is het elegantste.
+        for( ActionListener l : actionListenerList)
+            l.actionPerformed( e );
+    }
 }
